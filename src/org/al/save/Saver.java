9e4ade8f325fa57b9d39211fb2bf676719efbe1f
AG8 <lhscompsci@lh-l703-06-7359.ci.lexington.ma.us>
2016-06-01 10:32:27 -0400

    Decreased savefile size

--- src/org/al/save/Saver.java
+++ src/org/al/save/Saver.java
@@ -30,7 +30,7 @@ public class Saver implements Runnable {
 
         for (ImmutablePair<MiniBoard, Integer> keyPair : toSave.keySet()) {
             ImmutablePair<Integer, Integer> valuePair = toSave.get(keyPair);
-            writer.println(line++ + "a:^^^" + keyPair.getLeft().toSaveString() + "^^^:a~~~~~b:^^^" + keyPair.getRight() + "^^^:b~~~~~c:^^^" + valuePair.getLeft() + "^^^:c~~~~~d:^^^" + valuePair.getRight() + "^^^:d");
+            writer.println(line++ + "a:^^^" + keyPair.getLeft().toSaveString() + "^^^:a b:^^^" + keyPair.getRight() + "^^^:b c:^^^" + valuePair.getLeft() + "^^^:c d:^^^" + valuePair.getRight() + "^^^:d");
         }
 
         writer.close();
