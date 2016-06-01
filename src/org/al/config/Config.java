11c4dff24d9401ee5a89e2dab6b15bd92be24b1f
AG8 <lhscompsci@lh-l703-06-7359.ci.lexington.ma.us>
2016-06-01 10:43:58 -0400

    Larger Q matrix

--- src/org/al/config/Config.java
+++ src/org/al/config/Config.java
@@ -1,7 +1,7 @@
 package org.al.config;
 
 public class Config {
-    public static final int TOTAL_RUNS = 100000;
+    public static final int TOTAL_RUNS = 1000000;
     public static final int SAVE_CHUNK = 10000;
 
     public static final boolean DISPLAY_GAME = false;
